659b7340571e4eed87f6e9eafdd94da1a3482a55
Wagner Andrade <wagnerandrade_df@hotmail.com>
2025-03-15 12:38:08 -0300

    Tests product repository - Delete ID

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dscatalog.entities.Product;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		
+		long exintingId = 1L;
+		
+		repository.deleteById(exintingId);
+		
+		Optional<Product> result = repository.findById(exintingId);
+		Assertions.assertFalse(result.isPresent());
+		
+	}
+
+}
