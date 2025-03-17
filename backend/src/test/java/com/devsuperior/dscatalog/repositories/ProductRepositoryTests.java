dfbad8d5b41f735f3258f4bfab5be918eb37894e
Wagner Andrade <wagnerandrade_df@hotmail.com>
2025-03-17 15:46:01 -0300

    Fixtures no JUnit, BeforeEach

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,28 +3,44 @@ package com.devsuperior.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
 
 @DataJpaTest
 public class ProductRepositoryTests {
-	
+
 	@Autowired
 	private ProductRepository repository;
+	
+	private long existingId;
+	private long nonExistingId;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+	}
 	
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
 		
-		long exintingId = 1L;
-		
-		repository.deleteById(exintingId);
-		
-		Optional<Product> result = repository.findById(exintingId);
-		Assertions.assertFalse(result.isPresent());
-		
-	}
+		repository.deleteById(existingId);
 
+		Optional<Product> result = repository.findById(existingId);
+		
+		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);			
+		});
+	}
 }
