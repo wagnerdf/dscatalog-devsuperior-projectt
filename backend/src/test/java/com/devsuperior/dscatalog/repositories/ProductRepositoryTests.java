c0fc7b5d410ec1dc5d874b65b8820ce87099d18b
Wagner Andrade <wagnerandrade_df@hotmail.com>
2025-03-17 16:54:39 -0300

    Testing save with null ID

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -19,11 +20,25 @@ public class ProductRepositoryTests {
 	
 	private long existingId;
 	private long nonExistingId;
+	private long countTotalProducts;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+	@Test
+	public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+
+		Product product = Factory.createProduct();
+		product.setId(null);
+		
+		product = repository.save(product);
+		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1L, product.getId());
+
 	}
 	
 	@Test
@@ -44,3 +59,10 @@ public class ProductRepositoryTests {
 		});
 	}
 }
+
+
+
+
+
+
+
