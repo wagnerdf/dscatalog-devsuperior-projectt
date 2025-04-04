79bea85da04610ca02f24a7edcd9c58555586aac
Wagner Andrade <wagnerandrade_df@hotmail.com>
2025-04-04 09:58:59 -0300

    Handling MethodArgumentNotValidException exception

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandle.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandle.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -37,6 +38,19 @@ public class ResourceExceptionHandle {
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("msg - Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY; /* O retorno de código de erro 422 */
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation exception");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		
