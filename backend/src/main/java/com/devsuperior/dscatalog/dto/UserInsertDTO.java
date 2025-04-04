abb17ab7f230b25d3b7bced05df63eb8c50f5609
Wagner Andrade <wagnerandrade_df@hotmail.com>
2025-04-04 11:15:39 -0300

    Implementing a custom ConstraintValidator

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dscatalog.dto;
 
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid /* Anotation criada em service.validation */
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 	
