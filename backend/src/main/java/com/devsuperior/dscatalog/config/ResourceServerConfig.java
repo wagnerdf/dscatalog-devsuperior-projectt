79363a574fec5c04908de9c2454eecdb2eab23e1
Wagner Andrade <wagnerandrade_df@hotmail.com>
2025-04-09 11:58:20 -0300

    Special configuration for H2 bench

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,9 +17,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private JwtTokenStore tokenStore; 
 	
-	private static final String[] PUBLIC = { "/oauth/token" };
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };	
 	
@@ -26,9 +32,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
 	}
-
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		// Liberar o H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")){
+			http.headers().frameOptions().disable();
+			
+		}
+		
 		http.authorizeRequests() //iniciar as configurações de autorizações
 		.antMatchers(PUBLIC).permitAll() //Define autoriza~çoes de rota PUBLIC
 		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll() //Liberar o metodo GET para o vetor OPERATOR_OR_ADMIN
